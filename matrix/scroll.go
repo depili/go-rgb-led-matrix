e7ae7afb5a3528206705ac48adcbc575059ad08b
Vesa-Pekka Palmu <vpalmu@depili.fi>
2016-07-25 21:05:37 +0300

    Add ScrollPlasma
    Scrolls the bitmask using colors from the plasma effect

--- matrix/scroll.go
+++ matrix/scroll.go
@@ -2,19 +2,33 @@ package matrix
 
 import ()
 
-func (matrix *matrix) Scroll(bitmask [][]bool, color [3]byte, y int, x int, offset int, length int) {
+func (matrix *matrix) Scroll(bitmask [][]bool, color [3]byte, y, x, offset, length int) {
+	matrix.DrawBitmask(matrix.getScrollBitmask(bitmask, y, x, offset, length), color, y, x)
+}
+
+func (matrix *matrix) ScrollPlasma(bitmask [][]bool, y, x, offset, length int) {
+	matrix.DrawBitmapMask(matrix.PlasmaBitmap(offset),
+		matrix.getScrollBitmask(bitmask, y, x, offset, length), y, x)
+}
+
+func (matrix *matrix) getScrollBitmask(bitmask [][]bool, y, x, offset, length int) [][]bool {
 	s := len(bitmask[0])
 	if s < length {
-		matrix.DrawBitmask(bitmask, color, y, x)
-		return
+		return bitmask
 	}
 	// Seamlessly loop the bitmap around
 	offset = offset % s
 	size := s - offset
 	if size >= length {
-		matrix.DrawBitmask(matrix.OffsetBitmask(bitmask, offset, length), color, y, x)
+		return matrix.OffsetBitmask(bitmask, offset, length)
 	} else {
-		matrix.DrawBitmask(matrix.OffsetBitmask(bitmask, offset, length), color, y, x)
-		matrix.DrawBitmask(matrix.OffsetBitmask(bitmask, 0, length-size), color, y, x+size)
+		ret := make([][]bool, len(bitmask))
+		for r, row := range matrix.OffsetBitmask(bitmask, offset, length) {
+			ret[r] = row
+		}
+		for r, row := range matrix.OffsetBitmask(bitmask, 0, length-size) {
+			ret[r] = append(ret[r], row...)
+		}
+		return ret
 	}
 }
